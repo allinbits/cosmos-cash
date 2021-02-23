6b32afcd0044675da8b24470edba043e33e33d77
PaddyMc <paddymchale@hotmail.com>
2021-02-23 16:07:49 +0000

    feat(did): query for individual did document

--- x/identifier/keeper/grpc_query.go
+++ x/identifier/keeper/grpc_query.go
@@ -29,3 +29,27 @@ func (q Keeper) Identifiers(
 		DidDocuments: identifiers,
 	}, nil
 }
+
+// Identifers implements the Identifers gRPC method
+func (q Keeper) Identifier(
+	c context.Context,
+	req *types.QueryIdentifierRequest,
+) (*types.QueryIdentifierResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "empty request")
+	}
+
+	if req.Id == "" {
+		return nil, status.Error(codes.InvalidArgument, "verifiable credential id cannot be empty")
+	}
+
+	ctx := sdk.UnwrapSDKContext(c)
+	identifier, found := q.GetIdentifier(ctx, []byte(req.Id))
+	if !found {
+		return nil, status.Error(codes.NotFound, "identifier not found: QueryIdentifier")
+	}
+
+	return &types.QueryIdentifierResponse{
+		identifier,
+	}, nil
+}
--- x/identifier/types/query.pb.go
+++ x/identifier/types/query.pb.go
@@ -140,40 +140,138 @@ func (m *QueryIdentifiersResponse) GetPagination() *query.PageResponse {
 	return nil
 }
 
+// QueryIdentifersRequest is request type for Query/Identifers RPC method.
+type QueryIdentifierRequest struct {
+	// status enables to query for validators matching a given status.
+	Id string `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty"`
+}
+
+func (m *QueryIdentifierRequest) Reset()         { *m = QueryIdentifierRequest{} }
+func (m *QueryIdentifierRequest) String() string { return proto.CompactTextString(m) }
+func (*QueryIdentifierRequest) ProtoMessage()    {}
+func (*QueryIdentifierRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_5eda05580bf44a1c, []int{2}
+}
+func (m *QueryIdentifierRequest) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *QueryIdentifierRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_QueryIdentifierRequest.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *QueryIdentifierRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_QueryIdentifierRequest.Merge(m, src)
+}
+func (m *QueryIdentifierRequest) XXX_Size() int {
+	return m.Size()
+}
+func (m *QueryIdentifierRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_QueryIdentifierRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_QueryIdentifierRequest proto.InternalMessageInfo
+
+func (m *QueryIdentifierRequest) GetId() string {
+	if m != nil {
+		return m.Id
+	}
+	return ""
+}
+
+// QueryIdentifersResponse is response type for the Query/Identifers RPC method
+type QueryIdentifierResponse struct {
+	// validators contains all the queried validators.
+	DidDocuments DidDocument `protobuf:"bytes,1,opt,name=didDocuments,proto3" json:"didDocuments"`
+}
+
+func (m *QueryIdentifierResponse) Reset()         { *m = QueryIdentifierResponse{} }
+func (m *QueryIdentifierResponse) String() string { return proto.CompactTextString(m) }
+func (*QueryIdentifierResponse) ProtoMessage()    {}
+func (*QueryIdentifierResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_5eda05580bf44a1c, []int{3}
+}
+func (m *QueryIdentifierResponse) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *QueryIdentifierResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_QueryIdentifierResponse.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *QueryIdentifierResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_QueryIdentifierResponse.Merge(m, src)
+}
+func (m *QueryIdentifierResponse) XXX_Size() int {
+	return m.Size()
+}
+func (m *QueryIdentifierResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_QueryIdentifierResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_QueryIdentifierResponse proto.InternalMessageInfo
+
+func (m *QueryIdentifierResponse) GetDidDocuments() DidDocument {
+	if m != nil {
+		return m.DidDocuments
+	}
+	return DidDocument{}
+}
+
 func init() {
 	proto.RegisterType((*QueryIdentifiersRequest)(nil), "allinbits.cosmoscash.identifier.QueryIdentifiersRequest")
 	proto.RegisterType((*QueryIdentifiersResponse)(nil), "allinbits.cosmoscash.identifier.QueryIdentifiersResponse")
+	proto.RegisterType((*QueryIdentifierRequest)(nil), "allinbits.cosmoscash.identifier.QueryIdentifierRequest")
+	proto.RegisterType((*QueryIdentifierResponse)(nil), "allinbits.cosmoscash.identifier.QueryIdentifierResponse")
 }
 
 func init() { proto.RegisterFile("identifier/query.proto", fileDescriptor_5eda05580bf44a1c) }
 
 var fileDescriptor_5eda05580bf44a1c = []byte{
-	// 392 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x9c, 0x92, 0xc1, 0x4a, 0xe3, 0x40,
-	0x1c, 0xc6, 0x33, 0xdd, 0xdd, 0xc2, 0x4e, 0xf7, 0x34, 0x2c, 0xdd, 0xd2, 0xd5, 0xb4, 0x14, 0xd4,
-	0x22, 0x3a, 0x43, 0xdb, 0x8b, 0x5e, 0x4b, 0x51, 0x3c, 0x08, 0x9a, 0x83, 0x07, 0x6f, 0x93, 0x74,
-	0x4c, 0x07, 0xda, 0x99, 0xb4, 0x33, 0x11, 0x73, 0xf5, 0x09, 0x04, 0x9f, 0xc2, 0xb3, 0x47, 0x5f,
-	0xa0, 0xc7, 0x82, 0x17, 0x4f, 0x22, 0xad, 0x0f, 0x22, 0xc9, 0x84, 0x26, 0xa2, 0x52, 0xf0, 0xf6,
-	0x25, 0xf3, 0x7d, 0xdf, 0xfc, 0xfe, 0xc3, 0x1f, 0x96, 0x79, 0x9f, 0x09, 0xcd, 0x2f, 0x38, 0x9b,
-	0x90, 0x71, 0xc8, 0x26, 0x11, 0x0e, 0x26, 0x52, 0x4b, 0x54, 0xa3, 0xc3, 0x21, 0x17, 0x2e, 0xd7,
-	0x0a, 0x7b, 0x52, 0x8d, 0xa4, 0xf2, 0xa8, 0x1a, 0xe0, 0xcc, 0x5c, 0x5d, 0xf3, 0xa5, 0xf4, 0x87,
-	0x8c, 0xd0, 0x80, 0x13, 0x2a, 0x84, 0xd4, 0x54, 0x73, 0x29, 0x94, 0x89, 0x57, 0xb7, 0x4d, 0x88,
-	0xb8, 0x54, 0x31, 0xd3, 0x4b, 0x2e, 0x5b, 0x2e, 0xd3, 0xb4, 0x45, 0x02, 0xea, 0x73, 0x91, 0x98,
-	0x53, 0xef, 0xff, 0x1c, 0x42, 0x26, 0xd3, 0xc3, 0xbf, 0xbe, 0xf4, 0x65, 0x22, 0x49, 0xac, 0xcc,
-	0xdf, 0x46, 0x04, 0xff, 0x9d, 0xc6, 0xa5, 0x47, 0x4b, 0xbb, 0x72, 0xd8, 0x38, 0x64, 0x4a, 0xa3,
-	0x32, 0x2c, 0x2a, 0x4d, 0x75, 0xa8, 0x2a, 0xa0, 0x0e, 0x9a, 0xbf, 0x9d, 0xf4, 0x0b, 0x1d, 0x40,
-	0x98, 0xdd, 0x5c, 0x29, 0xd4, 0x41, 0xb3, 0xd4, 0xde, 0x4c, 0x67, 0xc3, 0x31, 0x26, 0x36, 0xe3,
-	0xa7, 0x98, 0xf8, 0x84, 0xfa, 0x2c, 0xed, 0x74, 0x72, 0xc9, 0xc6, 0x03, 0x80, 0x95, 0x8f, 0x77,
-	0xab, 0x40, 0x0a, 0xc5, 0xd0, 0x19, 0xfc, 0xd3, 0xe7, 0xfd, 0x9e, 0xf4, 0xc2, 0x11, 0x13, 0x3a,
-	0x46, 0xf8, 0xd1, 0x2c, 0xb5, 0x77, 0xf0, 0x8a, 0xc7, 0xc4, 0xbd, 0x2c, 0xd4, 0xfd, 0x39, 0x7d,
-	0xae, 0x59, 0xce, 0xbb, 0x1e, 0x74, 0xf8, 0x09, 0xfc, 0xd6, 0x4a, 0x78, 0x03, 0x95, 0xa7, 0x6f,
-	0xdf, 0x03, 0xf8, 0x2b, 0xa1, 0x47, 0x77, 0x00, 0x96, 0x72, 0x23, 0xa0, 0xbd, 0x95, 0x90, 0x5f,
-	0xbc, 0x78, 0x75, 0xff, 0x1b, 0x49, 0x83, 0xd6, 0xd8, 0xb8, 0x7e, 0x7c, 0xbd, 0x2d, 0xd4, 0xd0,
-	0x3a, 0x59, 0x56, 0xa4, 0x2b, 0xa0, 0xa3, 0xa5, 0x50, 0xdd, 0xe3, 0xe9, 0xdc, 0x06, 0xb3, 0xb9,
-	0x0d, 0x5e, 0xe6, 0x36, 0xb8, 0x59, 0xd8, 0xd6, 0x6c, 0x61, 0x5b, 0x4f, 0x0b, 0xdb, 0x3a, 0xef,
-	0xf8, 0x5c, 0x0f, 0x42, 0x17, 0x7b, 0x72, 0x94, 0xab, 0x30, 0x14, 0xbb, 0x31, 0x06, 0xb9, 0xca,
-	0xed, 0x14, 0xd1, 0x51, 0xc0, 0x94, 0x5b, 0x4c, 0x96, 0xa8, 0xf3, 0x16, 0x00, 0x00, 0xff, 0xff,
-	0xcf, 0x50, 0xd5, 0xc5, 0xfc, 0x02, 0x00, 0x00,
+	// 450 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xac, 0x93, 0xc1, 0x8a, 0xd3, 0x40,
+	0x1c, 0xc6, 0x33, 0x51, 0x17, 0x9c, 0x8a, 0x87, 0x41, 0x6a, 0xa9, 0x92, 0x5d, 0x23, 0xac, 0x51,
+	0x74, 0x86, 0xed, 0x1e, 0x5c, 0xaf, 0xcb, 0xa2, 0x78, 0x10, 0x34, 0x07, 0x0f, 0xde, 0x26, 0xc9,
+	0x98, 0x0e, 0xb4, 0x33, 0x69, 0x67, 0x22, 0x16, 0xf1, 0xe2, 0x13, 0x08, 0x3e, 0x87, 0xf8, 0x00,
+	0xbe, 0x40, 0x8f, 0x05, 0x2f, 0x9e, 0x44, 0x5a, 0x2f, 0xbe, 0x85, 0x24, 0x33, 0x6d, 0x52, 0x5b,
+	0x0d, 0x15, 0x6f, 0x93, 0xe4, 0xff, 0x7d, 0xff, 0xdf, 0xf7, 0x31, 0x81, 0x6d, 0x9e, 0x30, 0xa1,
+	0xf9, 0x4b, 0xce, 0xc6, 0x64, 0x94, 0xb3, 0xf1, 0x04, 0x67, 0x63, 0xa9, 0x25, 0xda, 0xa7, 0x83,
+	0x01, 0x17, 0x11, 0xd7, 0x0a, 0xc7, 0x52, 0x0d, 0xa5, 0x8a, 0xa9, 0xea, 0xe3, 0x6a, 0xb8, 0x7b,
+	0x3d, 0x95, 0x32, 0x1d, 0x30, 0x42, 0x33, 0x4e, 0xa8, 0x10, 0x52, 0x53, 0xcd, 0xa5, 0x50, 0x46,
+	0xde, 0xbd, 0x63, 0x44, 0x24, 0xa2, 0x8a, 0x19, 0x5f, 0xf2, 0xea, 0x28, 0x62, 0x9a, 0x1e, 0x91,
+	0x8c, 0xa6, 0x5c, 0x94, 0xc3, 0x76, 0xf6, 0x5a, 0x0d, 0xa1, 0x3a, 0xda, 0x8f, 0x57, 0x52, 0x99,
+	0xca, 0xf2, 0x48, 0x8a, 0x93, 0x79, 0xeb, 0x4f, 0xe0, 0xd5, 0x67, 0x85, 0xe9, 0xe3, 0xd5, 0xb8,
+	0x0a, 0xd9, 0x28, 0x67, 0x4a, 0xa3, 0x36, 0xdc, 0x53, 0x9a, 0xea, 0x5c, 0x75, 0xc0, 0x01, 0x08,
+	0x2e, 0x86, 0xf6, 0x09, 0x3d, 0x84, 0xb0, 0xda, 0xdc, 0x71, 0x0f, 0x40, 0xd0, 0xea, 0x1d, 0xda,
+	0x6c, 0xb8, 0xc0, 0xc4, 0x26, 0xbe, 0xc5, 0xc4, 0x4f, 0x69, 0xca, 0xac, 0x67, 0x58, 0x53, 0xfa,
+	0x9f, 0x01, 0xec, 0x6c, 0xee, 0x56, 0x99, 0x14, 0x8a, 0xa1, 0xe7, 0xf0, 0x52, 0xc2, 0x93, 0x33,
+	0x19, 0xe7, 0x43, 0x26, 0x74, 0x81, 0x70, 0x2e, 0x68, 0xf5, 0xee, 0xe2, 0x86, 0x32, 0xf1, 0x59,
+	0x25, 0x3a, 0x3d, 0x3f, 0xfd, 0xb6, 0xef, 0x84, 0x6b, 0x3e, 0xe8, 0xd1, 0x16, 0xf8, 0x5b, 0x8d,
+	0xf0, 0x06, 0x6a, 0x8d, 0x3e, 0x80, 0xed, 0xdf, 0xe0, 0x97, 0xbd, 0x5d, 0x86, 0x2e, 0x4f, 0x6c,
+	0x67, 0x2e, 0x4f, 0xfc, 0xd1, 0x46, 0xc5, 0x7f, 0x49, 0x09, 0xfe, 0x47, 0xca, 0xde, 0x4f, 0x17,
+	0x5e, 0x28, 0x77, 0xa2, 0x8f, 0x00, 0xb6, 0x6a, 0xfd, 0xa2, 0x93, 0x46, 0xef, 0x3f, 0x5c, 0x87,
+	0xee, 0x83, 0x7f, 0x50, 0x9a, 0x98, 0xfe, 0xed, 0x77, 0x5f, 0x7e, 0x7c, 0x70, 0x6f, 0xa2, 0x1b,
+	0x64, 0x65, 0x41, 0xb6, 0x5e, 0x55, 0x85, 0x3e, 0x01, 0x08, 0x2b, 0x0b, 0x74, 0x7f, 0xd7, 0xa5,
+	0x4b, 0xda, 0x93, 0xdd, 0x85, 0x16, 0x16, 0x97, 0xb0, 0x01, 0x3a, 0x6c, 0x84, 0x25, 0x6f, 0x78,
+	0xf2, 0xf6, 0xf4, 0xc9, 0x74, 0xee, 0x81, 0xd9, 0xdc, 0x03, 0xdf, 0xe7, 0x1e, 0x78, 0xbf, 0xf0,
+	0x9c, 0xd9, 0xc2, 0x73, 0xbe, 0x2e, 0x3c, 0xe7, 0xc5, 0x71, 0xca, 0x75, 0x3f, 0x8f, 0x70, 0x2c,
+	0x87, 0x35, 0x2f, 0x43, 0x73, 0xaf, 0xc0, 0x21, 0xaf, 0xeb, 0xce, 0x7a, 0x92, 0x31, 0x15, 0xed,
+	0x95, 0xff, 0xe5, 0xf1, 0xaf, 0x00, 0x00, 0x00, 0xff, 0xff, 0x93, 0xef, 0x9f, 0xc4, 0x4f, 0x04,
+	0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -190,6 +288,7 @@ const _ = grpc.SupportPackageIsVersion4
 type QueryClient interface {
 	// Identifers queries all validators that match the given status.
 	Identifiers(ctx context.Context, in *QueryIdentifiersRequest, opts ...grpc.CallOption) (*QueryIdentifiersResponse, error)
+	Identifier(ctx context.Context, in *QueryIdentifierRequest, opts ...grpc.CallOption) (*QueryIdentifierResponse, error)
 }
 
 type queryClient struct {
@@ -209,10 +308,20 @@ func (c *queryClient) Identifiers(ctx context.Context, in *QueryIdentifiersReque
 	return out, nil
 }
 
+func (c *queryClient) Identifier(ctx context.Context, in *QueryIdentifierRequest, opts ...grpc.CallOption) (*QueryIdentifierResponse, error) {
+	out := new(QueryIdentifierResponse)
+	err := c.cc.Invoke(ctx, "/allinbits.cosmoscash.identifier.Query/Identifier", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // QueryServer is the server API for Query service.
 type QueryServer interface {
 	// Identifers queries all validators that match the given status.
 	Identifiers(context.Context, *QueryIdentifiersRequest) (*QueryIdentifiersResponse, error)
+	Identifier(context.Context, *QueryIdentifierRequest) (*QueryIdentifierResponse, error)
 }
 
 // UnimplementedQueryServer can be embedded to have forward compatible implementations.
@@ -221,6 +330,9 @@ type UnimplementedQueryServer struct {
 
 func (*UnimplementedQueryServer) Identifiers(ctx context.Context, req *QueryIdentifiersRequest) (*QueryIdentifiersResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Identifiers not implemented")
+}
+func (*UnimplementedQueryServer) Identifier(ctx context.Context, req *QueryIdentifierRequest) (*QueryIdentifierResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Identifier not implemented")
 }
 
 func RegisterQueryServer(s grpc1.Server, srv QueryServer) {
@@ -245,6 +357,24 @@ func _Query_Identifiers_Handler(srv interface{}, ctx context.Context, dec func(i
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Query_Identifier_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(QueryIdentifierRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(QueryServer).Identifier(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/allinbits.cosmoscash.identifier.Query/Identifier",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(QueryServer).Identifier(ctx, req.(*QueryIdentifierRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _Query_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "allinbits.cosmoscash.identifier.Query",
 	HandlerType: (*QueryServer)(nil),
@@ -252,6 +382,10 @@ var _Query_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "Identifiers",
 			Handler:    _Query_Identifiers_Handler,
+		},
+		{
+			MethodName: "Identifier",
+			Handler:    _Query_Identifier_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
@@ -349,6 +483,69 @@ func (m *QueryIdentifiersResponse) MarshalToSizedBuffer(dAtA []byte) (int, error
 	return len(dAtA) - i, nil
 }
 
+func (m *QueryIdentifierRequest) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *QueryIdentifierRequest) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *QueryIdentifierRequest) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if len(m.Id) > 0 {
+		i -= len(m.Id)
+		copy(dAtA[i:], m.Id)
+		i = encodeVarintQuery(dAtA, i, uint64(len(m.Id)))
+		i--
+		dAtA[i] = 0xa
+	}
+	return len(dAtA) - i, nil
+}
+
+func (m *QueryIdentifierResponse) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *QueryIdentifierResponse) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *QueryIdentifierResponse) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	{
+		size, err := m.DidDocuments.MarshalToSizedBuffer(dAtA[:i])
+		if err != nil {
+			return 0, err
+		}
+		i -= size
+		i = encodeVarintQuery(dAtA, i, uint64(size))
+	}
+	i--
+	dAtA[i] = 0xa
+	return len(dAtA) - i, nil
+}
+
 func encodeVarintQuery(dAtA []byte, offset int, v uint64) int {
 	offset -= sovQuery(v)
 	base := offset
@@ -393,6 +590,30 @@ func (m *QueryIdentifiersResponse) Size() (n int) {
 		l = m.Pagination.Size()
 		n += 1 + l + sovQuery(uint64(l))
 	}
+	return n
+}
+
+func (m *QueryIdentifierRequest) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	l = len(m.Id)
+	if l > 0 {
+		n += 1 + l + sovQuery(uint64(l))
+	}
+	return n
+}
+
+func (m *QueryIdentifierResponse) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	l = m.DidDocuments.Size()
+	n += 1 + l + sovQuery(uint64(l))
 	return n
 }
 
@@ -616,6 +837,171 @@ func (m *QueryIdentifiersResponse) Unmarshal(dAtA []byte) error {
 				m.Pagination = &query.PageResponse{}
 			}
 			if err := m.Pagination.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipQuery(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthQuery
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *QueryIdentifierRequest) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowQuery
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: QueryIdentifierRequest: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: QueryIdentifierRequest: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Id", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowQuery
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthQuery
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthQuery
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Id = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipQuery(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthQuery
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *QueryIdentifierResponse) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowQuery
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: QueryIdentifierResponse: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: QueryIdentifierResponse: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field DidDocuments", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowQuery
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthQuery
+			}
+			postIndex := iNdEx + msglen
+			if postIndex < 0 {
+				return ErrInvalidLengthQuery
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			if err := m.DidDocuments.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
 				return err
 			}
 			iNdEx = postIndex

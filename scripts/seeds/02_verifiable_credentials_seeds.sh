8878f9162701e24b849e016afc65128075784df7
PaddyMc <paddymchale@hotmail.com>
2021-02-26 09:51:46 +0000

    feat(seeds): add query and validate commands to vsc seeds

--- scripts/seeds/02_verifiable_credentials_seeds.sh
+++ scripts/seeds/02_verifiable_credentials_seeds.sh
@@ -2,3 +2,10 @@
 
 echo "Creating verifiable credential for user :validator"
 cosmos-cashd tx verifiablecredentialservice create-verifiable-credential did:cash:$(cosmos-cashd keys show validator -a) --from validator --chain-id cash -y
+
+echo "Querying verifiable credentials"
+cosmos-cashd query verifiablecredentialservice verifiable-credentials --output json | jq
+
+echo "Validating verifiable credentials"
+cosmos-cashd query verifiablecredentialservice validate-verifiable-credential new-verifiable-cred-3 $(cosmos-cashd keys show validator -p) --output json | jq
+

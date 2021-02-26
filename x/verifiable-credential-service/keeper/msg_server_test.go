b9eeb71efe142e3540ab429f1fc6fa6fd953c34e
PaddyMc <paddymchale@hotmail.com>
2021-02-26 11:34:30 +0000

    feat(tests): increase vcs keeper test coverage

---
+++ x/verifiable-credential-service/keeper/msg_server_test.go
@@ -0,0 +1,76 @@
+package keeper
+
+import (
+	"fmt"
+
+	"github.com/allinbits/cosmos-cash/x/verifiable-credential-service/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func (suite *KeeperTestSuite) TestMsgSeverCreateVerifableCredential() {
+	server := NewMsgServerImpl(suite.keeper)
+	var req types.MsgCreateVerifiableCredential
+
+	testCases := []struct {
+		msg      string
+		malleate func()
+		expPass  bool
+	}{
+		{
+			"correctly creates vc",
+			func() {
+				cs := types.NewCredentialSubject(
+					"accAddr",
+					true,
+				)
+
+				vc := types.NewVerifiableCredential(
+					"new-verifiable-cred-3",
+					[]string{"VerifiableCredential", "KYCCredential"},
+					"accAddr",
+					fmt.Sprintf("%s", "currentTome"),
+					cs,
+					types.NewProof("", "", "", "", ""),
+				)
+				req = *types.NewMsgCreateVerifiableCredential(vc, "did:cash:1111")
+			},
+			true,
+		},
+		{
+			"vc already exists",
+			func() {
+				cs := types.NewCredentialSubject(
+					"accAddr",
+					true,
+				)
+
+				vc := types.NewVerifiableCredential(
+					"new-verifiable-cred-3",
+					[]string{"VerifiableCredential", "KYCCredential"},
+					"accAddr",
+					fmt.Sprintf("%s", "currentTome"),
+					cs,
+					types.NewProof("", "", "", "", ""),
+				)
+				suite.keeper.SetVerifiableCredential(suite.ctx, []byte(vc.Id), vc)
+
+				req = *types.NewMsgCreateVerifiableCredential(vc, "did:cash:1111")
+			},
+			false,
+		},
+	}
+	for _, tc := range testCases {
+		suite.Run(fmt.Sprintf("Case %s", tc.msg), func() {
+			tc.malleate()
+
+			vcResp, err := server.CreateVerifiableCredential(sdk.WrapSDKContext(suite.ctx), &req)
+			if tc.expPass {
+				suite.NoError(err)
+				suite.NotNil(vcResp)
+
+			} else {
+				suite.Require().Error(err)
+			}
+		})
+	}
+}

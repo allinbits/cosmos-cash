6e79d706c6ccbb6d905ecf324f61587cb6384dc1
PaddyMc <paddymchale@hotmail.com>
2021-02-26 11:34:30 +0000

    feat(tests): increase vcs keeper test coverage

--- scripts/seeds/00_start_chain.sh
+++ scripts/seeds/00_start_chain.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
 
-echo "Initialising chain"
-cosmos-cashd init --chain-id=cash cash 
-echo "y" | cosmos-cashd keys add validator
+GENESIS_FILE=~/.cosmoscash/config/genesis.json
+if [ -f $GENESIS_FILE ]
+then
+    echo "Genesis file exist, would you like to delete it? (y/n)"
+    read delete_config
+fi
 
-echo "Adding genesis account"
-cosmos-cashd add-genesis-account $(cosmos-cashd keys show validator -a) 1000000000stake
-cosmos-cashd gentx validator 700000000stake --chain-id cash
-cosmos-cashd collect-gentxs 
+if [[
+	$delete_config == "Y" ||
+	$delete_config == "y" ||
+	! -f $GENESIS_FILE
+   ]];
+then
+    rm -r ~/.cosmoscash
+
+    echo "Initialising chain"
+    cosmos-cashd init --chain-id=cash cash
+    echo "y" | cosmos-cashd keys add validator
+
+    echo "Adding genesis account"
+    cosmos-cashd add-genesis-account $(cosmos-cashd keys show validator -a) 1000000000stake
+    cosmos-cashd gentx validator 700000000stake --chain-id cash
+    cosmos-cashd collect-gentxs
+fi
+
 
 echo "Starting cosmos cash chain"
-cosmos-cashd start 
+cosmos-cashd start
+
---
+++ x/verifiable-credential-service/keeper/verifiable-credential_test.go
@@ -0,0 +1,58 @@
+package keeper
+
+import (
+	"fmt"
+	"github.com/allinbits/cosmos-cash/x/verifiable-credential-service/types"
+)
+
+func (suite *KeeperTestSuite) TestVerifiableCredentialsKeeperSetAndGet() {
+	testCases := []struct {
+		msg string
+		vc  types.VerifiableCredential
+		// TODO: add mallate func and clean up test
+		expPass bool
+	}{
+		{
+			"data stored successfully",
+			types.NewVerifiableCredential(
+				"did:cash:1111",
+				[]string{"context"},
+				"",
+				"",
+				types.NewCredentialSubject("", true),
+				types.NewProof("", "", "", "", ""),
+			),
+			true,
+		},
+	}
+	for _, tc := range testCases {
+		suite.keeper.SetVerifiableCredential(
+			suite.ctx,
+			[]byte(tc.vc.Id),
+			tc.vc,
+		)
+		suite.keeper.SetVerifiableCredential(
+			suite.ctx,
+			[]byte(tc.vc.Id+"1"),
+			tc.vc,
+		)
+		suite.Run(fmt.Sprintf("Case %s", tc.msg), func() {
+			if tc.expPass {
+				_, found := suite.keeper.GetVerifiableCredential(
+					suite.ctx,
+					[]byte(tc.vc.Id),
+				)
+				suite.Require().True(found)
+
+				array := suite.keeper.GetAllVerifiableCredentials(
+					suite.ctx,
+				)
+
+				suite.Require().Equal(2, len(array))
+			} else {
+				// TODO write failure cases
+				suite.Require().False(tc.expPass)
+			}
+		})
+	}
+}

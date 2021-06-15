2a74cdf5af0a9dc36cbc4a5f6b6d1ecf66605841
PaddyMc <paddymchale@hotmail.com>
2021-06-15 11:58:48 +0100

    feat(kyc-cred): ante handler for kyc credentials

--- x/verifiable-credential-service/client/cli/tx.go
+++ x/verifiable-credential-service/client/cli/tx.go
@@ -29,6 +29,7 @@ func GetTxCmd() *cobra.Command {
 	cmd.AddCommand(
 		NewCreateUserVerifiableCredentialCmd(),
 		NewCreateIssuerVerifiableCredentialCmd(),
+		NewCreateKYCVerifiableCredentialCmd(),
 	)
 
 	return cmd
@@ -37,7 +38,7 @@ func GetTxCmd() *cobra.Command {
 // NewCreateUserVerifiableCredentialCmd defines the command to create a new verifiable credential.
 func NewCreateUserVerifiableCredentialCmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:     `create-verifiable-credential [did_url] [cred-id] [secret] [name] [address] [date_of_birth] [nationalId] [phoneNumber]`,
+		Use:     `create-verifiable-credential [did_url] [cred_id] [secret] [name] [address] [date_of_birth] [nationalId] [phoneNumber]`,
 		Short:   "create decentralized verifiable-credential",
 		Example: "creates a verifiable credential for users",
 		Args:    cobra.ExactArgs(8),
@@ -53,7 +54,7 @@ func NewCreateUserVerifiableCredentialCmd() *cobra.Command {
 			secret := args[2]
 
 			inputs := args[3:8]
-			// Data for the tree
+
 			data := make([][]byte, len(inputs))
 			for i, v := range inputs {
 				data[i] = []byte(v)
@@ -76,7 +77,7 @@ func NewCreateUserVerifiableCredentialCmd() *cobra.Command {
 
 			vc := types.NewUserVerifiableCredential(
 				args[1],
-				[]string{"VerifiableCredential", "KYCCredential"},
+				[]string{"VerifiableCredential", "IdentityCredential"},
 				accAddrBech32,
 				tm.Format(time.RFC3339),
 				cs,
@@ -122,7 +123,7 @@ func NewCreateIssuerVerifiableCredentialCmd() *cobra.Command {
 	// TODO: investigate add a file for schema
 	// can we use a file to allow only having one command
 	cmd := &cobra.Command{
-		Use:     `create-issuer-verifiable-credential [did-url] [cred-id] [secret] [business-name] [business-registration-number] [business-type] [business-address]`,
+		Use:     `create-issuer-verifiable-credential [did-url] [cred_id] [secret] [business-name] [business-registration-number] [business-type] [business-address]`,
 		Short:   "create decentralized verifiable-credential for issuer",
 		Example: "creates a verifiable credential for issuers",
 		Args:    cobra.ExactArgs(7),
@@ -162,6 +163,89 @@ func NewCreateIssuerVerifiableCredentialCmd() *cobra.Command {
 			vc := types.NewUserVerifiableCredential(
 				args[1],
 				[]string{"VerifiableCredential", "IssuerCredential"},
+				accAddrBech32,
+				tm.Format(time.RFC3339),
+				cs,
+				types.NewProof("", "", "", "", ""),
+			)
+
+			// TODO: this could be expensive review this signing method
+			// TODO: we can hash this an make this less expensive
+			signature, pubKey, err := clientCtx.Keyring.SignByAddress(accAddr, vc.GetBytes())
+			if err != nil {
+				return err
+			}
+
+			p := types.NewProof(
+				pubKey.Type(),
+				tm.Format(time.RFC3339),
+				"assertionMethod",
+				accAddrBech32+"#keys-1",
+				base64.StdEncoding.EncodeToString(signature),
+			)
+			vc.Proof = &p
+
+			msg := types.NewMsgCreateVerifiableCredential(
+				vc,
+				accAddrBech32,
+			)
+
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
+
+// NewCreateKYCVerifiableCredentialCmd defines the command to create a new verifiable credential.
+func NewCreateKYCVerifiableCredentialCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     `create-kyc-verifiable-credential [did_url] [cred_id] [secret] [amount_per_transaction] [total_number_of_transactions] [total_transaction_amount]`,
+		Short:   "create decentralized verifiable-credential",
+		Example: "creates a verifiable credential for users",
+		Args:    cobra.ExactArgs(6),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+			//cdc := codec.NewProtoCodec(clientCtx.InterfaceRegistry)
+			accAddr := clientCtx.GetFromAddress()
+			accAddrBech32 := accAddr.String()
+
+			secret := args[2]
+
+			inputs := args[3:6]
+
+			data := make([][]byte, len(inputs))
+			for i, v := range inputs {
+				data[i] = []byte(v)
+			}
+
+			tree, err := merkletree.NewUsing(data, New(secret), nil)
+			if err != nil {
+				return err
+			}
+
+			root := tree.Root()
+			hexRoot := hex.EncodeToString(root)
+
+			cs := types.NewUserCredentialSubject(
+				args[0],
+				hexRoot,
+				true,
+			)
+			tm := time.Now()
+
+			vc := types.NewUserVerifiableCredential(
+				args[1],
+				[]string{"VerifiableCredential", "KYCCredential"},
 				accAddrBech32,
 				tm.Format(time.RFC3339),
 				cs,

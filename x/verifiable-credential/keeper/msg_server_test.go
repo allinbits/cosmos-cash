b47b455e64548d53b5b49d07edf4fd269f747955
PaddyMc <paddymchale@hotmail.com>
2021-09-02 11:32:38 +0100

    feat(creds): test delete verifiable credential function

--- x/verifiable-credential/keeper/msg_server_test.go
+++ x/verifiable-credential/keeper/msg_server_test.go
@@ -73,3 +73,61 @@ func (suite *KeeperTestSuite) TestMsgSeverCreateVerifableCredential() {
 		})
 	}
 }
+
+func (suite *KeeperTestSuite) TestMsgSeverDeleteVerifableCredential() {
+	server := NewMsgServerImpl(suite.keeper)
+	var req types.MsgDeleteVerifiableCredential
+
+	testCases := []struct {
+		msg      string
+		malleate func()
+		expPass  bool
+	}{
+		{
+			"correctly deletes vc",
+			func() {
+				cs := types.NewUserCredentialSubject(
+					"accAddr",
+					"root",
+					true,
+				)
+
+				vc := types.NewUserVerifiableCredential(
+					"new-verifiable-cred-3",
+					"did:cash:1111",
+					time.Now(),
+					cs,
+				)
+				suite.keeper.SetVerifiableCredential(suite.ctx, []byte(vc.Id), vc)
+
+				req = *types.NewMsgDeleteVerifiableCredential(vc.Id, vc.Issuer, "did:cash:1111")
+			},
+			true,
+		},
+		{
+			"vc does not exists",
+			func() {
+				req = *types.NewMsgDeleteVerifiableCredential(
+					"new-verifiable-cred-3",
+					"did:cosmos:cash:issuer",
+					"did:cash:1111",
+				)
+			},
+			false,
+		},
+	}
+	for _, tc := range testCases {
+		suite.Run(fmt.Sprintf("Case %s", tc.msg), func() {
+			tc.malleate()
+
+			vcResp, err := server.DeleteVerifiableCredential(sdk.WrapSDKContext(suite.ctx), &req)
+			if tc.expPass {
+				suite.NoError(err)
+				suite.NotNil(vcResp)
+
+			} else {
+				suite.Require().Error(err)
+			}
+		})
+	}
+}
